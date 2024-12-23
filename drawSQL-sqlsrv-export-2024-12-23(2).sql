3af827ab91b244190c3c1772bf4ac80b8f8545b2
riandarafqi <riandarafqi321@gmail.com>
2024-12-23 12:53:07 +0700

    commit pertama

---
+++ drawSQL-sqlsrv-export-2024-12-23(2).sql
@@ -0,0 +1,40 @@
+CREATE TABLE "Departments"(
+    "IdDepartments" BIGINT NOT NULL,
+    "NamaDepartment" VARCHAR(255) NOT NULL,
+    "AlamatDepartments" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Departments" ADD CONSTRAINT "departments_iddepartments_primary" PRIMARY KEY("IdDepartments");
+CREATE TABLE "Karyawan"(
+    "IdKaryawan" BIGINT NOT NULL,
+    "IdDepartments" BIGINT NOT NULL,
+    "NamaKaryawan" VARCHAR(255) NOT NULL,
+    "TanggalLahir" DATE NOT NULL,
+    "Nomor_Telepon" INT NOT NULL,
+    "Alamat" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "Karyawan" ADD CONSTRAINT "karyawan_idkaryawan_primary" PRIMARY KEY("IdKaryawan");
+CREATE TABLE "Jabatan"(
+    "IdJabatan" BIGINT NOT NULL,
+    "IdKaryawan" BIGINT NOT NULL,
+    "NamaJabatan" VARCHAR(255) NOT NULL,
+    "StatusJabatan" NVARCHAR(255) CHECK
+        ("StatusJabatan" IN(N'')) NOT NULL
+);
+ALTER TABLE
+    "Jabatan" ADD CONSTRAINT "jabatan_idjabatan_primary" PRIMARY KEY("IdJabatan");
+CREATE TABLE "Gaji"(
+    "IdGaji" BIGINT NOT NULL,
+    "IdKaryawan" BIGINT NOT NULL,
+    "JumlahGaji" BIGINT NOT NULL,
+    "TanggalPengajian" DATE NOT NULL
+);
+ALTER TABLE
+    "Gaji" ADD CONSTRAINT "gaji_idgaji_primary" PRIMARY KEY("IdGaji");
+ALTER TABLE
+    "Karyawan" ADD CONSTRAINT "karyawan_iddepartments_foreign" FOREIGN KEY("IdDepartments") REFERENCES "Departments"("IdDepartments");
+ALTER TABLE
+    "Gaji" ADD CONSTRAINT "gaji_idkaryawan_foreign" FOREIGN KEY("IdKaryawan") REFERENCES "Karyawan"("IdKaryawan");
+ALTER TABLE
+    "Jabatan" ADD CONSTRAINT "jabatan_idkaryawan_foreign" FOREIGN KEY("IdKaryawan") REFERENCES "Karyawan"("IdKaryawan");
